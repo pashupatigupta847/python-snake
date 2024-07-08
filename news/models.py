92b6d10c84543f08010eda0023c6e5fd8f87e769
pashupatigupta847 <pashupatigupta748@gmail.com>
2024-07-08 08:55:03 -0700

    news model registered and media urls settings

--- news/models.py
+++ news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="news")
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "news"
+        verbose_name_plural = "news"
